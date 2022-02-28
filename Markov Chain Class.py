0a0fcde90fadf1d64e0e5fe87a4e41853f440a44
lucib3196 <lucianobmecheng69@gmail.com>
2022-02-28 12:34:10 -0800

    Update Markov Chain Class.py

--- Markov Chain Class.py
+++ Markov Chain Class.py
@@ -1,3 +1,4 @@
+from cgi import test
 import numpy as np
 import sympy as sym
 
@@ -6,11 +7,41 @@ class Markov:
         self.ss = system_size
 
     def set_transition(self, transition_probability):
-        if len(transition_probability) != (self.ss *self.ss):
-            return  ValueError("List too short." )
-        self.set_transition = transition_probability
+        if len(transition_probability) != (self.ss * self.ss):
+            raise  ValueError("List too short." )
+        self.transition = transition_probability
 
-Markov(3)
-Markov.set_transition([])
+    def get_transition_matrix(self):
+        self.P_0 = (np.array(self.transition)).reshape(self.ss,self.ss)
+        return self.P_0
+
+    def set_initial_pop(self,initial_pop):
+        X_n = np.array(initial_pop)
+        self.d = X_n.sum()
+        X_n = X_n/self.d
+        self.X_n = X_n.reshape(self.ss,1)
+    
+    def get_initial_pop_matrix(self):
+        return self.X_n
+
+    def set_years(self,year):
+        self.year = year
+
+    # Generalize This to Form for system size
+    def final_population(self):
+        for i in range(self.year):
+            Pop_final = np.matmul(self.P_0,self.X_n)
+        Pop_final = np.around(self.X_n*self.d)
+        return ( 'The Final Population after ', self.year, 'Years') + ('Population A: ' ,Pop_final[0,0])+('Population B: ' ,Pop_final[1,0])+('Population C: ' ,Pop_final[2,0])
 
 
+test = Markov(3)
+test.set_transition([0.70, 0.15, 0.15, 0.20, 0.80, 0.15,0.10,0.05,0.70])
+print(test.get_transition_matrix())
+test.set_initial_pop([15000,20000,65000])
+print(test.get_initial_pop_matrix())
+
+
+test.set_years(3)
+
+print(test.final_population())
